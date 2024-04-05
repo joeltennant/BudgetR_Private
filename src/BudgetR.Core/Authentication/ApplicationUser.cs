85b109abc1ee0bf1ac7da5a5e0e15baa678502fb
Joel Tennant <joel_tennant@yahoo.com>
2024-04-05 11:41:37 -0600

    Initial Setup

---
+++ src/BudgetR.Core/Authentication/ApplicationUser.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace BudgetR.Core.Authentication;
+// Add profile data for application users by adding properties to the ApplicationUser class
+public class ApplicationUser : IdentityUser
+{
+}
